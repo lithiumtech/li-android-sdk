4a8ae195265c57a430495ac62882d801e23aff2b
Sumit Pannalall <sumit.pannalall@lithium.com>
2017-05-25 15:39:40 +0530

    MOB-51: removed user context and author fields from subscriptions

--- li-android-sdk-core/src/main/java/lithium/community/android/sdk/utils/LiQueryConstant.java
+++ li-android-sdk-core/src/main/java/lithium/community/android/sdk/utils/LiQueryConstant.java
@@ -23,7 +23,7 @@ public class LiQueryConstant {
 
     // LiQueryConstant for LiClientManager
     public static final String LI_ARTICLES_CLIENT_BASE_LIQL = "SELECT id, subject, view_href, post_time, kudos.sum(weight), conversation.style, conversation.solved, conversation.last_post_time, user_context, author FROM messages";
-    public static final String LI_SUBSCRIPTIONS_CLIENT_BASE_LIQL = "SELECT id, target.id, target.subject, target.post_time, target.kudos.sum(weight), target.body, target.conversation.style, target.conversation.solved, target.conversation.last_post_time, user_context, author FROM subscriptions";
+    public static final String LI_SUBSCRIPTIONS_CLIENT_BASE_LIQL = "SELECT id, target.id, target.subject, target.post_time, target.kudos.sum(weight), target.body, target.conversation.style, target.conversation.solved, target.conversation.last_post_time FROM subscriptions";
     public static final String LI_BROWSE_CLIENT_BASE_LIQL = "select id, title, parent.id, parent.title, depth from nodes";
     public static final String LI_SEARCH_CLIENT_BASE_LIQL = "SELECT id, subject, view_href, board.id, post_time, kudos.sum(weight), conversation.style, conversation.solved, conversation.last_post_time, user_context, author FROM messages";
     public static final String LI_MESSAGE_CHILDREN_CLIENT_BASE_LIQL = "SELECT id, body, subject, view_href, post_time, kudos.sum(weight), can_accept_solution, is_solution, user_context.kudo, user_context.can_kudo, user_context.can_reply, user_context.read, author.id, author.avatar, author.login FROM messages";
