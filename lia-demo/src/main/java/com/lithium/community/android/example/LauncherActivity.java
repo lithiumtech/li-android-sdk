eb68fa1b73278b58c0ae91fe0b28f7cd024108e6
NarendraPrabhu <narendraprabhubg@gmail.com>
2018-08-09 10:18:50 +0530

    MOB-558 Added user-profile screen to Support Activity
    MOB-558 Added login/logout option
    
    MOB-464
    
    MOB-558 Added Login and Logout operations

---
+++ lia-demo/src/main/java/com/lithium/community/android/example/LauncherActivity.java
@@ -0,0 +1,7 @@
+package com.lithium.community.android.example;
+
+import com.lithium.community.android.ui.components.activities.LiSupportHomeActivity;
+
+public class LauncherActivity extends LiSupportHomeActivity {
+//DO NOTHING
+}
