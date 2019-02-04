3a51a6413cf5b9c0ef666bf7776385a16f98170c
NarendraPrabhu <narendraprabhubg@gmail.com>
2019-02-04 17:40:18 +0530

    Added more actions and image loading in notifications

--- lia-demo/src/main/java/com/lithium/community/android/example/NotificationBroadcastReceiver.java
+++ lia-demo/src/main/java/com/lithium/community/android/example/NotificationBroadcastReceiver.java
@@ -8,13 +8,25 @@ import android.os.Bundle;
 import android.support.v4.app.RemoteInput;
 import android.widget.Toast;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.lithium.community.android.api.LiClient;
 import com.lithium.community.android.exception.LiRestResponseException;
 import com.lithium.community.android.manager.LiClientManager;
+import com.lithium.community.android.manager.LiSDKManager;
 import com.lithium.community.android.model.request.LiClientRequestParams;
+import com.lithium.community.android.model.response.LiUser;
 import com.lithium.community.android.rest.LiAsyncRequestCallback;
+import com.lithium.community.android.rest.LiBaseResponse;
 import com.lithium.community.android.rest.LiBaseRestRequest;
+import com.lithium.community.android.rest.LiGetClientResponse;
 import com.lithium.community.android.rest.LiPostClientResponse;
+import com.lithium.community.android.ui.components.adapters.LiConversationAdapter;
+import com.lithium.community.android.ui.components.utils.LiUIUtils;
+
+import java.net.HttpURLConnection;
+
+import static com.lithium.community.android.example.ReferenceFirebaseMessagingService.KEY_USER_ID;
 
 public class NotificationBroadcastReceiver extends BroadcastReceiver {
 
@@ -24,7 +36,6 @@ public class NotificationBroadcastReceiver extends BroadcastReceiver {
         Long messageId = intent.getLongExtra(ReferenceFirebaseMessagingService.KEY_MESSAGE_ID, 0);
         int notificationId = intent.getIntExtra(ReferenceFirebaseMessagingService.KEY_NOTIFICATION_ID, 0);
         if (ReferenceFirebaseMessagingService.REPLY_ACTION.equals(intent.getAction())) {
-            // do whatever you want with the message. Send to the server or add to the db.
             String message = getReplyMessage(intent).toString();
             new ReplyThread(context, messageId, message, notificationId).start();
         }
@@ -35,6 +46,19 @@ public class NotificationBroadcastReceiver extends BroadcastReceiver {
 
         if (ReferenceFirebaseMessagingService.ACCEPT_SOLUTION_ACTION.equals(intent.getAction())) {
             new ASThread(messageId, context, notificationId).start();
+        }
+
+        if (ReferenceFirebaseMessagingService.ACTION_REPORT_ABUSE.equals(intent.getAction())) {
+            long userId = intent.getLongExtra(KEY_USER_ID, 0);
+            new ReportAbuseThread(context, messageId, userId, notificationId).start();
+        }
+
+        if (ReferenceFirebaseMessagingService.ACTION_SHARE.equals(intent.getAction())) {
+            shareContent(context, intent.getStringExtra(ReferenceFirebaseMessagingService.KEY_MESSAGE), notificationId);
+        }
+
+        if (ReferenceFirebaseMessagingService.ACTION_UNSUBSCRIBE.equals(intent.getAction())) {
+            new UnsubscribeThread(context, messageId, notificationId).start();
         }
     }
 
@@ -161,6 +185,122 @@ public class NotificationBroadcastReceiver extends BroadcastReceiver {
                 cancelNotification(context, notificationId);
             }
         }
+    }
+
+    private class ReportAbuseThread extends Thread {
+
+        private Long messageId;
+        private Long userId;
+        private Context context;
+        private int notificationId;
+
+        ReportAbuseThread(Context context, Long messageId, Long userId, int notificationId) {
+            this.messageId = messageId;
+            this.userId = userId;
+            this.context = context;
+            this.notificationId = notificationId;
+        }
+
+        @Override
+        public void run() {
+            LiClientRequestParams params = new LiClientRequestParams.LiReportAbuseClientRequestParams(context, messageId + "", userId + "", "Abusive comment");
+            try {
+                LiClient client = LiClientManager.getReportAbuseClient(params);
+                client.processAsync(new LiAsyncRequestCallback<LiPostClientResponse>() {
+                    @Override
+                    public void onSuccess(LiBaseRestRequest request, LiPostClientResponse response) throws LiRestResponseException {
+                        cancelNotification(context, notificationId);
+                    }
+
+                    @Override
+                    public void onError(Exception exception) {
+                        cancelNotification(context, notificationId);
+                    }
+                });
+            } catch (LiRestResponseException lrre) {
+                lrre.printStackTrace();
+                cancelNotification(context, notificationId);
+            }
+        }
+    }
+
+    private class UnsubscribeThread extends Thread {
+
+        private Context context;
+        private Long messageId;
+        private int notificationId;
+
+        UnsubscribeThread(Context context, Long messageId, int notificationId) {
+            this.context = context;
+            this.messageId = messageId;
+            this.notificationId = notificationId;
+        }
+
+        @Override
+        public void run() {
+            LiUser user = LiSDKManager.getInstance().getLoggedInUser();
+            LiClientRequestParams.LiUserMessageSusbscriptionRequestParans params
+                    = new LiClientRequestParams.LiUserMessageSusbscriptionRequestParans(context, "" + messageId, "" + user.getId());
+            try {
+                LiClient client = LiClientManager.getUserMessageSubscriptionsClient(params);
+                client.processAsync(new LiAsyncRequestCallback<LiGetClientResponse>() {
+                    @Override
+                    public void onSuccess(LiBaseRestRequest request, LiGetClientResponse response) throws LiRestResponseException {
+                        if (response.getHttpCode() == HttpURLConnection.HTTP_OK || response.getHttpCode() == HttpURLConnection.HTTP_CREATED) {
+                            JsonObject responseObject = response.getJsonObject();
+                            if (!responseObject.has(LiBaseResponse.DATA)) {
+                                cancelNotification(context, notificationId);
+                                return;
+                            }
+                            JsonObject dataObject = responseObject.getAsJsonObject(LiBaseResponse.DATA);
+                            if (!dataObject.has(LiBaseResponse.ITEMS)) {
+                                cancelNotification(context, notificationId);
+                                return;
+                            }
+                            JsonArray array = dataObject.getAsJsonArray(LiBaseResponse.ITEMS);
+                            if (array == null || array.size() != 1) {
+                                cancelNotification(context, notificationId);
+                                return;
+                            }
+                            String id = array.get(0).getAsJsonObject().get(LiBaseResponse.ID).getAsString();
+                            LiClientRequestParams.LiDeleteSubscriptionParams deleteSubscriptionParams =
+                                    new LiClientRequestParams.LiDeleteSubscriptionParams(context, id);
+                            LiClient client = LiClientManager.getSubscriptionDeleteClient(deleteSubscriptionParams);
+                            client.processAsync(new LiAsyncRequestCallback<LiPostClientResponse>() {
+                                @Override
+                                public void onSuccess(LiBaseRestRequest request, LiPostClientResponse response) throws LiRestResponseException {
+                                    cancelNotification(context, notificationId);
+                                }
+
+                                @Override
+                                public void onError(Exception exception) {
+                                    cancelNotification(context, notificationId);
+                                }
+                            });
+                        } else {
+                            cancelNotification(context, notificationId);
+                        }
+                    }
+
+                    @Override
+                    public void onError(Exception exception) {
+                        cancelNotification(context, notificationId);
+                    }
+                });
+            } catch (LiRestResponseException lrre) {
+                lrre.printStackTrace();
+                cancelNotification(context, notificationId);
+            }
+        }
+    }
+
+    private void shareContent(Context context, String content, int notificationId) {
+        Intent intent = new Intent(Intent.ACTION_SEND_MULTIPLE);
+        intent.putExtra(Intent.EXTRA_TEXT, content);
+        intent.setType("text/plain");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity( Intent.createChooser(intent, "Share with..."));
+        cancelNotification(context, notificationId);
     }
 
     private void cancelNotification(Context context, int notificationId) {
--- lia-demo/src/main/java/com/lithium/community/android/example/ReferenceFirebaseMessagingService.java
+++ lia-demo/src/main/java/com/lithium/community/android/example/ReferenceFirebaseMessagingService.java
@@ -21,6 +21,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.RemoteInput;
 import android.text.Html;
@@ -47,7 +49,9 @@ import com.lithium.community.android.ui.components.utils.LiSDKConstants;
 import com.lithium.community.android.utils.LiCoreSDKConstants;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Map;
 
 /**
@@ -57,6 +61,7 @@ public class ReferenceFirebaseMessagingService extends FirebaseMessagingService 
 
     public static final String REPLY_ACTION = "action.reply";
     public static final String KEY_MESSAGE_ID = "message_id";
+    public static final String KEY_MESSAGE = "message";
     public static final String KEY_NOTIFICATION_ID = "notification_id";
     public static final int VALUE_NOTIFICATION_ID = 100001;
     public static final String KEY_REPLY = "Reply";
@@ -64,6 +69,13 @@ public class ReferenceFirebaseMessagingService extends FirebaseMessagingService 
     public static final String KEY_ACCEPT_SOLUTION = "Accept Solution";
     public static final String KUDO_ACTION = "action.Kudo";
     public static final String ACCEPT_SOLUTION_ACTION = "action.AS";
+    public static final String KEY_REPORT_ABUSE = "Report";
+    public static final String KEY_SHARE = "Share";
+    public static final String ACTION_REPORT_ABUSE = "action.RA";
+    public static final String ACTION_SHARE = "action.Share";
+    public static final String KEY_USER_ID = "AUTHOR_ID";
+    public static final String ACTION_UNSUBSCRIBE = "action.Unsubscribe";
+    public static final String KEY_UNSUBSCRIBE = "Unsubscribe";
 
 
     /**
@@ -119,7 +131,7 @@ public class ReferenceFirebaseMessagingService extends FirebaseMessagingService 
         new RetrieveMessageThread(messageID, fromName, replyMessageId, type).start();
     }
 
-    private void showNotification(String alertTitle, String contentText, String messageID, LiMessage message, LiMessage reply) {
+    private void showNotification(String alertTitle, String contentText, String messageID, LiMessage message, LiMessage reply, String type) {
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext())
                 .setContentInfo(reply == null ? message.getBody() : reply.getBody())
@@ -130,39 +142,112 @@ public class ReferenceFirebaseMessagingService extends FirebaseMessagingService 
                 .setStyle(new NotificationCompat.BigTextStyle().bigText(contentText))
                 .setSmallIcon(R.mipmap.ic_launcher);
 
-        if (reply != null) {
-            { //kudo action
+        switch (type) {
+            case "solutions" : {
                 Intent intent = new Intent(getApplicationContext(), NotificationBroadcastReceiver.class);
-                intent.setAction(KUDO_ACTION);
+                intent.setAction(ACTION_SHARE);
                 intent.putExtra(KEY_NOTIFICATION_ID, VALUE_NOTIFICATION_ID);
-                intent.putExtra(KEY_MESSAGE_ID, Long.valueOf(reply.getId()));
-                PendingIntent pi = PendingIntent.getBroadcast(getApplicationContext(), 100, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                builder.addAction(R.mipmap.ic_launcher, KEY_KUDO, pi);
-            }
-            { //accept solution action
-                Intent intent = new Intent(getApplicationContext(), NotificationBroadcastReceiver.class);
-                intent.setAction(ACCEPT_SOLUTION_ACTION);
-                intent.putExtra(KEY_NOTIFICATION_ID, VALUE_NOTIFICATION_ID);
-                intent.putExtra(KEY_MESSAGE_ID, Long.valueOf(reply.getId()));
-                PendingIntent pi = PendingIntent.getBroadcast(getApplicationContext(), 101, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                builder.addAction(R.mipmap.ic_launcher, KEY_ACCEPT_SOLUTION, pi);
-            }
-            { //reply action
-                RemoteInput remoteInput = new RemoteInput.Builder(KEY_REPLY)
-                        .setLabel(KEY_REPLY)
-                        .build();
-                Intent intent = new Intent(getApplicationContext(), NotificationBroadcastReceiver.class);
-                intent.setAction(REPLY_ACTION);
-                intent.putExtra(KEY_NOTIFICATION_ID, VALUE_NOTIFICATION_ID);
+                String data = String.format("Topic '%1$s' has a solution \n\n '%2$s'",
+                        message.getSubject(), Html.fromHtml(message.getBody()));
                 intent.putExtra(KEY_MESSAGE_ID, Long.valueOf(messageID));
-                PendingIntent pi = PendingIntent.getBroadcast(getApplicationContext(), 102, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                NotificationCompat.Action replyAction = new NotificationCompat.Action.Builder(
-                        R.drawable.ic_reply_black, KEY_REPLY, pi)
-                        .addRemoteInput(remoteInput)
-                        .setAllowGeneratedReplies(true)
-                        .build();
-                builder.addAction(replyAction);
+                intent.putExtra(KEY_MESSAGE, data);
+                PendingIntent pi = PendingIntent.getBroadcast(
+                        getApplicationContext(),
+                        101,
+                        intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.addAction(R.mipmap.ic_launcher, KEY_SHARE, pi);
             }
+            break;
+
+            case "kudos": {
+                {
+                    Intent intent = new Intent(getApplicationContext(), NotificationBroadcastReceiver.class);
+                    intent.setAction(ACTION_UNSUBSCRIBE);
+                    intent.putExtra(KEY_NOTIFICATION_ID, VALUE_NOTIFICATION_ID);
+                    intent.putExtra(KEY_MESSAGE_ID, Long.valueOf(messageID));
+                    PendingIntent pi = PendingIntent.getBroadcast(
+                            getApplicationContext(),
+                            102,
+                            intent,
+                            PendingIntent.FLAG_UPDATE_CURRENT);
+                    builder.addAction(R.mipmap.ic_launcher, KEY_UNSUBSCRIBE, pi);
+                }
+                /*{ //Report
+                    Intent intent = new Intent(getApplicationContext(), NotificationBroadcastReceiver.class);
+                    intent.setAction(ACTION_REPORT_ABUSE);
+                    intent.putExtra(KEY_NOTIFICATION_ID, VALUE_NOTIFICATION_ID);
+                    intent.putExtra(KEY_MESSAGE_ID, Long.valueOf(reply.getId()));
+                    intent.putExtra(KEY_USER_ID, reply.getAuthor().getId());
+                    PendingIntent pi = PendingIntent.getBroadcast(
+                            getApplicationContext(),
+                            103,
+                            intent,
+                            PendingIntent.FLAG_UPDATE_CURRENT);
+                    builder.addAction(R.mipmap.ic_launcher, KEY_REPORT_ABUSE, pi);
+                }*/
+            }
+            break;
+            default:
+                if (reply != null) {
+                    String imageUrl = null;
+                    if (reply.getBody().contains("<img src=\"")) {
+                        String value = reply.getBody().split("<img src=\"")[1];
+                        imageUrl = value.substring(0, value.indexOf("\"") - 1);
+                    }
+
+                    if (!TextUtils.isEmpty(imageUrl)) {
+                        NotificationCompat.BigPictureStyle style = new NotificationCompat.BigPictureStyle();
+                        style.setSummaryText(contentText);
+                        style.bigPicture(getBitmapFromURL(imageUrl));
+                        builder.setStyle(style);
+                    }
+
+                    { //kudo action
+                        Intent intent = new Intent(getApplicationContext(), NotificationBroadcastReceiver.class);
+                        intent.setAction(KUDO_ACTION);
+                        intent.putExtra(KEY_NOTIFICATION_ID, VALUE_NOTIFICATION_ID);
+                        intent.putExtra(KEY_MESSAGE_ID, Long.valueOf(reply.getId()));
+                        PendingIntent pi = PendingIntent.getBroadcast(
+                                getApplicationContext(),
+                                104,
+                                intent,
+                                PendingIntent.FLAG_UPDATE_CURRENT);
+                        builder.addAction(R.mipmap.ic_launcher, KEY_KUDO, pi);
+                    }
+                    { //accept solution action
+                        Intent intent = new Intent(getApplicationContext(), NotificationBroadcastReceiver.class);
+                        intent.setAction(ACCEPT_SOLUTION_ACTION);
+                        intent.putExtra(KEY_NOTIFICATION_ID, VALUE_NOTIFICATION_ID);
+                        intent.putExtra(KEY_MESSAGE_ID, Long.valueOf(reply.getId()));
+                        PendingIntent pi = PendingIntent.getBroadcast(
+                                getApplicationContext(),
+                                105,
+                                intent,
+                                PendingIntent.FLAG_UPDATE_CURRENT);
+                        builder.addAction(R.mipmap.ic_launcher, KEY_ACCEPT_SOLUTION, pi);
+                    }
+                    { //reply action
+                        RemoteInput remoteInput = new RemoteInput.Builder(KEY_REPLY)
+                                .setLabel(KEY_REPLY)
+                                .build();
+                        Intent intent = new Intent(getApplicationContext(), NotificationBroadcastReceiver.class);
+                        intent.setAction(REPLY_ACTION);
+                        intent.putExtra(KEY_NOTIFICATION_ID, VALUE_NOTIFICATION_ID);
+                        intent.putExtra(KEY_MESSAGE_ID, Long.valueOf(messageID));
+                        PendingIntent pi = PendingIntent.getBroadcast(
+                                getApplicationContext(),
+                                106,
+                                intent,
+                                PendingIntent.FLAG_UPDATE_CURRENT);
+                        NotificationCompat.Action replyAction = new NotificationCompat.Action.Builder(
+                                R.drawable.ic_reply_black, KEY_REPLY, pi)
+                                .addRemoteInput(remoteInput)
+                                .setAllowGeneratedReplies(true)
+                                .build();
+                        builder.addAction(replyAction);
+                    }
+                }
         }
 
         Intent notificationIntent = new Intent(this, LiConversationActivity.class);
@@ -177,6 +262,21 @@ public class ReferenceFirebaseMessagingService extends FirebaseMessagingService 
             manager.notify(VALUE_NOTIFICATION_ID, builder.build());
         } else {
             Log.e(getClass().getSimpleName(), "Notification Manager was null");
+        }
+    }
+
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            // Log exception
+            return null;
         }
     }
 
@@ -213,7 +313,7 @@ public class ReferenceFirebaseMessagingService extends FirebaseMessagingService 
                                     alertTitle = getString(R.string.li_message_kudo_notif_alert_text);
                                     replyMessageId = null;
                                     messageID = "" + message.getId();
-                                    showNotification(alertTitle, contentText, messageID, message, null);
+                                    showNotification(alertTitle, contentText, messageID, message, null, type);
                                     break;
                                 case "solutions":
                                     contentText = String.format(getString(R.string.li_message_solution_notif_content_text),
@@ -221,7 +321,7 @@ public class ReferenceFirebaseMessagingService extends FirebaseMessagingService 
                                     alertTitle = getString(R.string.li_message_solution_notif_alert_text);
                                     replyMessageId = null;
                                     messageID = "" + message.getId();
-                                    showNotification(alertTitle, contentText, messageID, message, null);
+                                    showNotification(alertTitle, contentText, messageID, message, null, type);
                                     break;
 
                                 default:
@@ -240,7 +340,7 @@ public class ReferenceFirebaseMessagingService extends FirebaseMessagingService 
                                                             name, message.getSubject(), Html.fromHtml(replyMessage.getBody()));
                                                     String alertTitle = getString(R.string.li_message_messages_notif_alert_text);
                                                     String messageID = "" + message.getId();
-                                                    showNotification(alertTitle, contentText, messageID, message, replyMessage);
+                                                    showNotification(alertTitle, contentText, messageID, message, replyMessage, type);
                                                 }
                                             }
 
